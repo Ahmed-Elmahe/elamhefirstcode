19c4eef1eacc178b41fef84c527fbb72af602c23
Ahmed-Elmahe <OfficialAlmahui@gmail.com>
2024-05-25 23:28:17 +0300

    my first code

---
+++ main.cs
@@ -0,0 +1,12 @@
+using system;
+
+namespace main 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.writeLine("Hello World!");
+        }
+    }
+}
